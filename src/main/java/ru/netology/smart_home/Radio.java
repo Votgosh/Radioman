fcfbeafc42f1545745a213273f2df8e07033352d
Votgosh <99820455+Votgosh@users.noreply.github.com>
2022-04-17 15:12:06 +0400

    first commit

---
+++ src/main/java/ru/netology/smart_home/Radio.java
@@ -0,0 +1,53 @@
+package ru.netology.smart_home;
+
+public class Radio {
+    private int currentStatusStation;
+    private int currentStatusVolume;
+
+    public int getCurrentStatusStation() {
+        return currentStatusStation;
+    }
+
+    public void setCurrentStatusStation(int currentStatusStation) {
+        this.currentStatusStation = currentStatusStation;
+    }
+
+    public void next() {
+        this.currentStatusStation = currentStatusStation + 1;
+        if (currentStatusStation == 10) {
+            this.currentStatusStation = 0;
+        }
+    }
+
+    public void stationIn(int stationIn) {
+        if (stationIn < 0) {
+            return;
+        }
+        if (stationIn > 9) {
+            return;
+        }
+        this.currentStatusStation = stationIn;
+    }
+
+    public int getCurrentStatusVolume() {
+        return currentStatusVolume;
+    }
+
+    public void setCurrentStatusVolume(int currentStatusVolume) {
+        this.currentStatusVolume = currentStatusVolume;
+    }
+
+    public void volumeUp() {//прибавление
+        this.currentStatusVolume = currentStatusVolume + 1;
+        if (currentStatusVolume == 11) {
+            this.currentStatusVolume = 10;
+        }
+    }
+
+    public void volumeDown() {//убавление
+        this.currentStatusVolume = currentStatusVolume - 1;
+        if (currentStatusVolume == -1) {
+            this.currentStatusVolume = 0;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/smart_home/RadioTest.java
@@ -0,0 +1,36 @@
+package ru.netology.smart_home;
+
+class RadioTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrentStatusStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentStatusStation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void next() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stationIn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCurrentStatusVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCurrentStatusVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void volumeUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void volumeDown() {
+    }
+}
